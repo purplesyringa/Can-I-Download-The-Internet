75095334bdf75165c34243fc33567ee99cc2d108
Ivanq <imachug@yandex.ru>
2019-08-17 20:58:14 +0300

    Save ZeroTalk

--- download-site.js
+++ download-site.js
@@ -1,6 +1,56 @@
+const {ZimCreator} = require("@openzim/libzim");
+
 const address = process.argv[2];
 
+process.stdin.resume(); // Don't sleep
+
 (async () => {
-	const zf = await require("./zeroframe")(address);
-	console.log(await zf.cmdp("siteInfo"));
+	try {
+		console.log(`Site:          ${address}`);
+
+		const zf = await require("./zeroframe")(address);
+
+		const siteInfo = await zf.cmdp("siteInfo");
+		console.log(`Address:       ${siteInfo.address}`);
+		console.log(`Title:         ${siteInfo.content.title}`);
+		console.log(`Description:   ${siteInfo.content.description}`);
+
+		let dbschema = await zf.cmdp("fileGet", ["dbschema.json"]);
+		try {
+			dbschema = JSON.parse(dbschema);
+		} catch(e) {
+			dbschema = null;
+		}
+		console.log(`Database:      ${dbschema ? dbschema.db_file : "none"}`);
+
+		// Detect site type
+		let siteType = "Unknown";
+		if(
+			siteInfo.content.description === "Decentralized forum with ZeroID" ||
+			(
+				dbschema &&
+				dbschema.db_name === "ZeroTalk" &&
+				dbschema.db_file === "data/users/zerotalk.db" &&
+				dbschema.tables &&
+				dbschema.tables.topic
+			)
+		) {
+			siteType = "ZeroTalk";
+		}
+
+		console.log(`Detected type: ${siteType}`);
+
+		const creator = new ZimCreator({fileName: `${address}.zim`, welcome: "index.html"});
+
+		const gather = require(`./gather/${siteType}`);
+		await gather(zf, creator);
+		await creator.finalise();
+
+		console.log("Done");
+
+		process.exit(0);
+	} catch(e) {
+		console.error(e);
+		process.exit(1);
+	}
 })();
---
+++ gather/Unknown.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+	console.log("Can't download unknown sites");
+};
